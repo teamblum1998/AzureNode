9c7cb3498c8ad123fb808d0d8d0c3165e29168e1
Derek Blum <derek.blum@ymail.com>
2019-09-28 13:59:26 -0400

    made for internet

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Mother F'ing World!");
+    response.end("Hello Monique... This is the first step!");
 });
 
 const port = process.env.PORT || 1337;
